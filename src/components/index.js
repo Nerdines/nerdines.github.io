526fec664695c1d2cecef18991ba20128c2e162a
DeathsValentine <39684769+DeathsValentine@users.noreply.github.com>
2022-12-28 21:47:49 -0800

    test imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import * as ReactDOM from 'react-dom';
 var ReactDOM = require('react-dom');
 
-const title = ReactDOM.createRoot(document.getElementById('root'));
+const title = ReactDOM.createRoot(document.getElementById('title'));
 title.render(<h1>Hello, world!</h1>);
