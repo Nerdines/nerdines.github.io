2b60fd330ba75852dd1dbf8d98df01e1671bfb0a
DeathsValentine <39684769+DeathsValentine@users.noreply.github.com>
2022-12-28 21:55:00 -0800

    react please work

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,4 @@
 import * as ReactDOM from 'react-dom';
-var ReactDOM = require('react-dom');
 
 const title = ReactDOM.createRoot(document.getElementById('title'));
 title.render(<h1>Hello, world!</h1>);
