faf30dcd87f08bd56f63c4acbc445eb3ceb27115
DeathsValentine <39684769+DeathsValentine@users.noreply.github.com>
2022-12-28 23:58:33 -0800

    Home page

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,16 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Header extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1 style={({textAlign: 'center'}, {fontFamily: 'Quicksand'})}>;
-                    Nerdines
-                </h1>
-            </div>
-        );
-    }
-}
-
-ReactDOM(<Header />, document.getElementById('header'));
