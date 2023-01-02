545f7efae2fbb455124c9d4f5c4a45360c65ee47
Eduardo Abreu <abreu.eduardo.r@gmail.com>
2023-01-01 23:13:25 -0800

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eduard-o.github.io',
-  integrations: [tailwind(), sitemap(), mdx(), prefetch()]
+  site: 'https://nerdines.github.io',
+  integrations: [tailwind(), sitemap(), mdx(), prefetch()],
 });
